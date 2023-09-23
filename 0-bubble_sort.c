7e47769552ec08506f3d45e118bb1983fe73a48d
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-09-23 15:53:55 -0400

    mainly updated the sort.h file and added 0-bubble_sort.c file

---
+++ 0-bubble_sort.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+* bubble_sort - this function impliments bubble sort algorithm
+ * after each swap the curent the curent array is printed
+* @array: the array of integers to be sorted
+* @size: size of the array
+* Return: void
+*/
+void bubble_sort(int *array, size_t size)
+{
+}
