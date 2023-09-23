ca730ce9b6bf52e1c9d078db608f3da864d12773
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-09-23 19:29:59 -0400

    new update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,4 +10,28 @@
 */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j;
+	int temp;
+	int swap_flag;
+
+	if (size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		swap_flag = 0;
+		for (j = 0; j + 1 < size - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swap_flag = 1;
+				print_array(array, size);
+			}
+
+		}
+		if (swap_flag == 0)
+			return;
+	}
 }
