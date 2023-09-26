bbc9c800fa4f3d0b19fcab26b2fb0f7a46b2b8f6
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-09-25 22:02:50 -0700

    Shell sort

---
+++ 100-sheel_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * shell_sort -> Function that sorrs arrays of integer
+ * in ascending order using tge shell sort algorithm
+ * using the knoth sequence
+ * @array: List of integers
+ * @size: Length of array
+ * Return: nothing
+ */
+
+void shell_sort(int *array, size_t size)
+{
+}
